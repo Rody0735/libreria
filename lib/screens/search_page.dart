c3b41e519849897a22fd78a704c3878c5b1b89cf
Rodolfo Diana <r.diana@studenti.unimol.it>
2024-05-25 12:47:18 +0200

    struct of the search_page

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:libreria/models/book.dart';
+import 'package:libreria/services/api.dart';
 
 class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+  Future<List<Book>>? _books;
+  final TextEditingController _controller = TextEditingController();
+  bool _searchByTitle = true;
+  bool _hasSearched = false;
 
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Search'),
+        ),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+
+  void search(String title) {
+    setState(() {
+      _books = searchBooks(title, _searchByTitle);
+    });
   }
 }
